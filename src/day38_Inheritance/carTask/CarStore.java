a642dc5a4f9c7ab66973377be7b6669e79deaead
cross <crosstown11223@yahoo.com>
2022-01-13 11:18:19 -0500

    day 38: Inheritance Method Overidding

--- src/day38_Inheritance/carTask/CarStore.java
+++ src/day38_Inheritance/carTask/CarStore.java
@@ -2,6 +2,11 @@ package day38_Inheritance.carTask;
 
 public class CarStore {
 
-     Tesla tesla1 = new Tesla("Extra", 2010,123000.0,"Back",123);
+     Tesla tesla = new Tesla("Extra", 2010,123000.0,"Back",123);
+     Toyota toyota = new Toyota("Carolla",2020,23000.0,"White",234);
+     BMW bmw = new BMW("7",2009,20000.0,"Green",234);
+
+
+
 
 }
