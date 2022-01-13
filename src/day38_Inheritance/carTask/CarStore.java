8f8c366e8f0b9fea7033d62304087df412d45536
cross <crosstown11223@yahoo.com>
2022-01-13 12:55:50 -0500

    day 38: Inheritance Method Overidding

--- src/day38_Inheritance/carTask/CarStore.java
+++ src/day38_Inheritance/carTask/CarStore.java
@@ -3,25 +3,16 @@ package day38_Inheritance.carTask;
 public class CarStore {
      public static void main(String[] args) {
           Tesla tesla = new Tesla("Extra", 2010,123000.0,"Back",123);
-          System.out.println(tesla);
-          tesla.spacing();
-          tesla.drive();
-          tesla.start();
-          System.out.println("-----------------------------------------------------------------");
+
 
           Toyota toyota = new Toyota("Carolla",2020,23000.0,"White",234);
-          System.out.println(toyota);
-          toyota.start();
-          toyota.drive();
-          toyota.reliable();
-          System.out.println("--------------------------------------------------------------------");
 
           BMW bmw = new BMW("7",2009,20000.0,"Green",234);
-          System.out.println(bmw);
+
+
+          toyota.start();  // I want  to see different implementation
+          tesla.start();  //  I need to creat overriding method
           bmw.start();
-          bmw.racing();
-          bmw.breakDown();
-          bmw.drive();
 
 
      }
--- src/day38_Inheritance/carTask/Toyota.java
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -9,6 +9,21 @@ public class Toyota extends Car{
     public void reliable(){
         System.out.println(brand+ " "+model+ " is reliable");
     }
+    //@Override  you  can put  and chack if it's overrided!!!!!!!!!!!!!!
+    public void start(){  //  since in class Car public modifier, I can give public but NOT LESS
+     System.out.println("Twist the key to ignition to start "+ brand+ " "+model);
+    }
+    //  if  I  have protected mod.  I can give protected or public
+    //  if I have private modifier in Car Class  the method can not be overrided
+    // now we have ONE method with diff. implement. in memory
+    // how to we know which method wil get executed
+    // By Object!!!!!!!!!!
+    // we overriding if  different implementation is needed
+    // Shortcuts= right click -> override-> Intelej is asking you which methods are you going to Override
+    // we MUST  check Car Class
+
+
+
 
 }
 // toyota extra methods   reliable()
