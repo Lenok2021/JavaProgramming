4579b39da224c9261e38d0434b9b369b2de00081
cross <crosstown11223@yahoo.com>
2022-01-13 11:02:14 -0500

    day 38: Inheritance Method Overidding

--- src/day38_Inheritance/carTask/BMW.java
+++ src/day38_Inheritance/carTask/BMW.java
@@ -1,7 +1,7 @@
 package day38_Inheritance.carTask;
 
-public class Tesla extends Car{
-    public Tesla( String model, int year, double price, String color, int miles) {
+public class BMW extends Car{
+    public BMW(String model, int year, double price, String color, int miles) {
         super("BMW", model, year, price, color, miles);
 
 
