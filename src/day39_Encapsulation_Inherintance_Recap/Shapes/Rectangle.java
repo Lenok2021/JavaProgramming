f1548e34ea4d5f95edec38e07ed3031ff1e68548
cross <crosstown11223@yahoo.com>
2022-01-14 11:47:07 -0500

    day 38: Inheritance Method Overidding

---
+++ src/day39_Encapsulation_Inherintance_Recap/Shapes/Rectangle.java
@@ -0,0 +1,59 @@
+package day39_Encapsulation_Inherintance_Recap.Shapes;
+
+public class Rectangle extends Shapes{
+    // before apply extends keyword
+   //  first  we have to create attributes  that cannot be inherited
+
+    private double length,width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length>=0){
+            System.err.println("Invalid Length: "+ length);
+            System.exit(1);
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width>=0){
+            System.err.println("Invalid Width: "+ width);
+            System.exit(1);
+        }
+        this.width = width;
+    }
+
+    public Rectangle(double length, double width) {
+        super("Rectangle");
+        setLength(length);
+        setWidth(width);
+
+    }
+
+    @Override
+    public double area() {
+       return length*width;
+    }
+
+    @Override
+    public double perimeter() {
+      return (length + width) * 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "length=" + length +
+                ", width=" + width +
+                " Area ='" + area() + '\'' +
+                " Perimeter ='" + perimeter() + '\'' +
+                '}';
+    }
+}
