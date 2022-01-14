716d073aa5b13a53cdc671513a172af8acc13d81
cross <crosstown11223@yahoo.com>
2022-01-14 10:16:40 -0500

    day 38: Inheritance Method Overidding

---
+++ src/day39_Encapsulation_Inherintance_Recap/MethodOverloading.java
@@ -0,0 +1,17 @@
+package day39_Encapsulation_Inherintance_Recap;
+
+public class MethodOverloading {
+
+public void method1(){
+
+}
+private int method1(int a){
+    return a;
+}
+// method name same, different parameters
+    // access modifier, return type can be different
+    // we can overload private to public, does not matter
+
+
+
+}
