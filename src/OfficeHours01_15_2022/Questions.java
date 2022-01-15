cb623c87345a81d054bd9043f2d460ef7c306181
cross <crosstown11223@yahoo.com>
2022-01-15 11:32:32 -0500

    day 38: Inheritance Method Overidding

---
+++ src/OfficeHours01_15_2022/Questions.java
@@ -0,0 +1,18 @@
+package OfficeHours01_15_2022;
+
+public class Questions {
+
+/*
+    Task 1 Create project and send the project into github
+
+              1-Create a new project called "GitHubPractice"
+              2-Create `.gitignore` file at root directory
+              3-Create a package under `src` called `practiceGit`
+              4-Create a class under package called "Solution1"
+              5-Enable version control to create new repo
+              6-Create 4 method "add","multiply","subtract","divide"
+              7-After each creation of method make commit
+              8-Push your code in GitHub
+ */
+
+}
