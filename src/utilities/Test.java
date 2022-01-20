2dc4219bebea106bd84ea9f64fc23bac1ba5ef86
cross <crosstown11223@yahoo.com>
2022-01-20 12:01:22 -0500

    added space

--- src/utilities/Test.java
+++ src/utilities/Test.java
@@ -7,6 +7,6 @@ public class Test {
 
       //  System.out.println(ProtectedAccessModifier.name1); // default can not be visible outside the package
      //   System.out.println(ProtectedAccessModifier.name2); //we called protected modifier
-        //  the  only time when it is visible aoutside the pacjage when this class is a subslasss
+        //  the  only time when protected variable is visible outside the package when this class is a Subclass
     }
 }
