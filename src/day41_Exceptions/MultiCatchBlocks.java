5a535145f0ed8d4fa0f8635d071c2e563d0136b6
cross <crosstown11223@yahoo.com>
2022-01-21 15:38:12 -0500

    added space

--- src/day41_Exceptions/MultiCatchBlocks.java
+++ src/day41_Exceptions/MultiCatchBlocks.java
@@ -7,29 +7,46 @@ public class MultiCatchBlocks {
     public static void main(String[] args) {
 
         Employee employee = null;
-        System.out.println( employee.getSalary() );  // Null Pointer Exception
 
-        //  in order  to specify all possible Exceptions I created multiCatchBlock
+        try {
 
-        try{
-            System.out.println( employee.getSalary() );
-        }catch(IndexOutOfBoundsException e){
-            System.out.println("First catch block");
+            System.out.println(employee.getSalary()); // Null Pointer Exception
+
+        }catch (NullPointerException e){
+            System.out.println("First Catch Block");
             e.printStackTrace();
-
-        }catch(ArithmeticException e){
+        }catch(IndexOutOfBoundsException e){
             System.out.println("Second catch block");
             e.printStackTrace();
-
-        }catch(ClassCastException e){
+        }catch (ArithmeticException e){
             System.out.println("Third catch block");
             e.printStackTrace();
-        }catch (RuntimeException e){
+        }catch (ClassCastException e){
             System.out.println("Fourth catch block");
+            e.printStackTrace();
+        }catch (RuntimeException e){
+            System.out.println("Fifth catch block");
             e.printStackTrace();
         }
 
 
+        System.out.println("Tested Completed");
+
+
+        System.out.println("---------------------------------------------------");
+
+        try {
+            System.out.println("Java".charAt(-1));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
+
+
+
+
     }
 
+
 }
+
+
