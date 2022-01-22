1b6de4812a01fef1c5217ce91cc7def7313bf604
cross <crosstown11223@yahoo.com>
2022-01-22 12:23:48 -0500

    Question10

---
+++ src/OffiseHours01_22_2022/Question10/TestTv.java
@@ -0,0 +1,12 @@
+package OffiseHours01_22_2022.Question10;
+
+public class TestTv {
+
+
+    public static void main(String[] args) {
+        Tv tv = new Tv();
+        tv.start(); // line 3
+       // tv.init(); // line 4
+
+    }
+}
---
+++ src/OffiseHours01_22_2022/Question10/Tv.java
@@ -0,0 +1,15 @@
+package OffiseHours01_22_2022.Question10;
+
+public class Tv {
+    private void init() {
+        System.out.println("Initialized");
+    }
+
+    public void start () {
+        init(); // line 2
+        System.out.println("Started");
+    }
+
+
+
+}
