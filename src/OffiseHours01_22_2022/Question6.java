c177f5022c2f0c9ef7580a1723956f97820331c6
cross <crosstown11223@yahoo.com>
2022-01-22 10:46:30 -0500

    Question6.java

--- src/OffiseHours01_22_2022/Question6.java
+++ src/OffiseHours01_22_2022/Question6.java
@@ -1,5 +1,18 @@
 package OffiseHours01_22_2022;
-
+import java.util.Arrays;
 public class Question6 {
+    public static void main(String[] args) {
+
+        int [] arr = {4,1,0,-8,4,8};
+        int min = minNum(arr);
+    }
+
+
+
+
+    public static int minNum(int [] nums){
+        Arrays.sort(nums);
+        return nums[0];
+    }
 
 }
