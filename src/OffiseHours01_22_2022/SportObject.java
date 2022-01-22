413f15a39811e6c735034bb2ff79647e327b75c9
cross <crosstown11223@yahoo.com>
2022-01-22 10:42:58 -0500

    Sport.java

---
+++ src/OffiseHours01_22_2022/SportObject.java
@@ -0,0 +1,9 @@
+package OffiseHours01_22_2022;
+
+public class SportObject {
+    public static void main(String [] args) {
+        Sport volleyball = new Sport();
+        volleyball.makeSport ("Volleyball", 12, true);
+        System.out.println(volleyball);
+    }
+}
