ae7a80230e457a0b02d6c17ea3d25e8bd0316e54
cross <crosstown11223@yahoo.com>
2022-01-22 10:51:14 -0500

    WaterPond

---
+++ src/OffiseHours01_22_2022/Pond.java
@@ -0,0 +1,13 @@
+package OffiseHours01_22_2022;
+
+public class Pond {
+
+    static Water water;
+
+    /* line 5 */ {
+        //   water = new Water(color);
+    }
+
+
+
+    }
---
+++ src/OffiseHours01_22_2022/Water.java
@@ -0,0 +1,11 @@
+package OffiseHours01_22_2022;
+
+
+
+    public class Water {
+        String color;
+
+        public Water(String color) {
+            this.color = color;
+        }
+}
