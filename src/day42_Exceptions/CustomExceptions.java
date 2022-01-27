23290082357cd0d41587a2fd5574f90affa66e61
cross <crosstown11223@yahoo.com>
2022-01-27 13:00:10 -0500

    ThrowKeyWord

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -1,14 +1,18 @@
 package day42_Exceptions;
 
-class FadyException extends RuntimeException{
+class FadyException extends RuntimeException {
 
-    public FadyException(){
+    public FadyException() {
         super("Time for a short break");
     }
 
-    public FadyException(String message){
-       super(message);
+    public FadyException(String message) {
+        super(message);
     }
+}
+
+class NoBreakException extends Exception {
+
 }
 
 //  we can creat  Exc. by myself
@@ -18,14 +22,19 @@ public class CustomExceptions {
 
     public static void main(String[] args) {
 
-        throw new FadyException("It's time for break");
+        // throw new FadyException("It's time for break");
+        throw new FadyException();
+        // when we have 2 Constructor we have two ways  how to create OB
+        //
 
+
+         // throw new NoBreakException();
+        // we get error BC is a checked Exc.
+        // we don't use Throw KeyWord for checked Exc.
+        // checked Exc/ does not have is a relationship with RunTime Class
 
 
     }
-
-
-
 
 
 }
