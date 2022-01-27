fd99f3db63eceec08f1e2b4c7e3d6bb309ee97c1
cross <crosstown11223@yahoo.com>
2022-01-27 11:47:21 -0500

    Question13

---
+++ src/day42_Exceptions/ThrowsKeyWord2.java
@@ -0,0 +1,21 @@
+package day42_Exceptions;
+
+public class ThrowsKeyWord2 {
+
+    public static void main(String[] args) throws ArithmeticException{
+
+        //  we use throws keyword only for checked exceptions
+        // below we have unchecked exception even I applied
+        // throw keyword  still have exception
+
+        System.out.println("Test started");
+
+      //  System.out.println(8/0);
+
+        System.out.println("Test completed");
+
+
+
+
+    }
+}
