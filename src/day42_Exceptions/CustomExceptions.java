1efe0dd7ba5c6ed4b7500551c3c00eed2117662d
cross <crosstown11223@yahoo.com>
2022-01-27 12:52:57 -0500

    ThrowKeyWord

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -2,6 +2,10 @@ package day42_Exceptions;
 
 class FadyException extends RuntimeException{
 
+    public FadyException(){
+        super("Time for a short break");
+    }
+
     public FadyException(String message){
        super(message);
     }
