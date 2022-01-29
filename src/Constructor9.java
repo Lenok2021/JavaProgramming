e140810963d753f08f7ac5b7a4bfe403e1a07deb
cross <crosstown11223@yahoo.com>
2022-01-28 19:01:28 -0500

    ThrowKeyWord

---
+++ src/Constructor9.java
@@ -0,0 +1,22 @@
+public class Constructor9 {
+
+
+        Constructor9()
+        {
+            show();
+        }
+        void show()
+        {
+            System.out.println("JAM JAM");
+        }
+        public static void main(String[] args)
+        {
+            Constructor9 con = new Constructor9();
+           
+        }
+    }
+
+
+
+
+
--- src/day42_Exceptions/MorningWorkOut.java
+++ src/day42_Exceptions/MorningWorkOut.java
@@ -2,6 +2,9 @@ package day42_Exceptions;
 
 public class MorningWorkOut {
     public static void main(String[] args) {
+
+
+
         System.out.println("------------Push up started----------");
 
         for (int i = 1; i <= 30 ; i++) {
