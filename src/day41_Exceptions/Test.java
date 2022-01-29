0967f134828b7a23f0d670424510c75bb58a0ff0
cross <crosstown11223@yahoo.com>
2022-01-28 20:12:12 -0500

    ThrowKeyWord

--- src/day41_Exceptions/Test.java
+++ src/day41_Exceptions/Test.java
@@ -2,6 +2,21 @@ package day41_Exceptions;
 
 public class Test {
 
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hello  I am  here");
+
+
+
+
+    }
+
 
 
 }
