241b8843ca3939a0cf2c0f7a0a360b3bb911497e
cross <crosstown11223@yahoo.com>
2022-01-31 10:54:34 -0500

    ThrowKeyWord

--- src/day43_Abstruction/Trow_vs_Throws.java
+++ src/day43_Abstruction/Trow_vs_Throws.java
@@ -1,10 +1,19 @@
 package day43_Abstruction;
 
+import java.util.Scanner;
+
 public class Trow_vs_Throws {
 
     public static void main(String[] args) {
 
         System.out.println("Enter your score:");
+        int score = new Scanner(System.in).nextInt();
+          if(score<0 || score >100){
+              throw new RuntimeException("Invalid score: "+ score);
+          }
+
+
+
 
 
     }
