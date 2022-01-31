8bcd20ad4c7ebce64a3656e70ac7aefe6fc1b6f0
cross <crosstown11223@yahoo.com>
2022-01-31 16:34:15 -0500

    Employee Class

--- src/day43_Abstruction/Car/Employee/Tester.java
+++ src/day43_Abstruction/Car/Employee/Tester.java
@@ -1,8 +1,20 @@
 package day43_Abstruction.Car.Employee;
 
-public class Tester {
+public class Tester extends Employee {
 
+    public Tester(String name, int age, char gender, int ID, String jofTitle, double salary) {
+        super(name, age, gender, ID, jofTitle, salary);
+    }
 
+    @Override
+    public void work() {
+        System.out.println(getName()+ " is testing the application");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName() + " sleeps 8 hours");
+    }
 
 
 
