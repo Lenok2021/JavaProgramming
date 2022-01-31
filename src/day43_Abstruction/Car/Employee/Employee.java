a400ce5f277115928dea9a9e0e0f54a1f39f70b1
cross <crosstown11223@yahoo.com>
2022-01-31 16:03:30 -0500

    Employee Class

---
+++ src/day43_Abstruction/Car/Employee/Employee.java
@@ -0,0 +1,48 @@
+package day43_Abstruction.Car.Employee;
+
+public abstract class Employee extends Person {
+    private final int ID;
+    private String jofTitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int ID, String jofTitle, double salary) {
+        super(name, age, gender);
+        this.ID = ID;
+        this.jofTitle = jofTitle;
+        this.salary = salary;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getJofTitle() {
+        return jofTitle;
+    }
+
+    public void setJofTitle(String jofTitle) {
+        this.jofTitle = jofTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public abstract void work();
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name"+getName() +
+                ", age=" + getAge() +
+                ", gender=" + getGender() +
+                "ID=" + ID +
+                ", jofTitle='" + jofTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
