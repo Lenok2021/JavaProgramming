b346fcd9418466a0e9ce51e9d4623ee268170619
cross <crosstown11223@yahoo.com>
2022-01-31 12:59:44 -0500

    AbstractClass

--- src/day43_Abstruction/Car/Honda.java
+++ src/day43_Abstruction/Car/Honda.java
@@ -1,5 +1,14 @@
 package day43_Abstruction.Car;
 
-public class Honda {
+public  class Honda extends Car {
 
+
+    public Honda(String color, String model, String make, int year, double price) {
+        super(color, model, make, year, price);
+    }
+
+    @Override
+    public  void start(){
+        System.out.println("Twist the key to ignition");
+    }
 }
