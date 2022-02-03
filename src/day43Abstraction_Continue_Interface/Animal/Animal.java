b1f30a85a0523d7e54483fd018c4f3e494d72cf8
cross <crosstown11223@yahoo.com>
2022-02-03 11:07:23 -0500

    Employee Class

--- src/day43Abstraction_Continue_Interface/Animal/Animal.java
+++ src/day43Abstraction_Continue_Interface/Animal/Animal.java
@@ -1,6 +1,6 @@
 package day43Abstraction_Continue_Interface.Animal;
 
-public class Animal {
+public abstract class Animal {
 
     private String name;
     //  if  you want something would be unchangeable you  can give final keyword
@@ -10,17 +10,20 @@ public class Animal {
     private String size;
     final private String color;
 
-    public final static  boolean canBreath;
+    public final static boolean canBreath;
 
 
-    static{
-        canBreath=true;
+    static {
+        canBreath = true;
     }
-
 
     public Animal(String name, String breed, char gender, int age, String size, String color) {
         setName(name);
         this.breed = breed;
+
+        if (!(gender == 'M' || gender == 'F')) {
+            throw new RuntimeException("Invalid gender " + gender);
+        }
         this.gender = gender;
         setAge(age);
         setSize(size);
@@ -32,6 +35,9 @@ public class Animal {
     }
 
     public void setName(String name) {
+        if (name.isEmpty()) {
+            throw new RuntimeException("Invalid name");
+        }
         this.name = name;
     }
 
@@ -48,6 +54,9 @@ public class Animal {
     }
 
     public void setAge(int age) {
+        if (age < 0) {
+            throw new RuntimeException("Invalid age " + age);
+        }
         this.age = age;
     }
 
@@ -61,6 +70,26 @@ public class Animal {
 
     public String getColor() {
         return color;
+    }
+
+    public abstract void eat();
+
+    public final void drink() {
+
+        System.out.println(name + " is drinking water");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
 
