953cbd8f9f07f1cbb9bd57e7a4c267904fd37912
cross <crosstown11223@yahoo.com>
2022-02-07 11:43:42 -0500

    Employee Class

---
+++ src/day44Abstraction_Continue_Interface/Animal/Flyable.java
@@ -0,0 +1,9 @@
+package day44Abstraction_Continue_Interface.Animal;
+
+public interface Flyable {
+
+    boolean canFly = true;
+
+    void fly();
+
+}
---
+++ src/day44Abstraction_Continue_Interface/Animal/Parrot.java
@@ -0,0 +1,29 @@
+package day44Abstraction_Continue_Interface.Animal;
+
+public class Parrot extends Animal implements Flyable{
+
+    public Parrot(String name, String breed, char gender, int age, String size, String color) {
+        super(name, breed, gender, age, size, color);
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("eating soup");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Flying very fast");
+    }
+
+    public static void main(String[] args) {
+        Parrot r = new Parrot("Bob","Dod",'M',5,"Small","Black");
+        r.fly();
+        r.eat();
+        r.drink();
+        System.out.println( Flyable.canFly );
+
+    }
+
+
+}
