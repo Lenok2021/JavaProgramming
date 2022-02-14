d7d9a8eba6fe281c2b57f5889adab66375e41a5b
cross <crosstown11223@yahoo.com>
2022-02-14 14:35:01 -0500

    commit

---
+++ src/day47_Pilymorphism_Continue/EncapsulationReview.java
@@ -0,0 +1,27 @@
+package day47_Pilymorphism_Continue;
+
+public class EncapsulationReview {
+
+     private  String bookTitle;
+     private  final String publishedData;  //  since final keyword we cannot create setter
+
+    public EncapsulationReview(String bookTitle, String publishedData) {
+        setBookTitle(bookTitle);
+        this.publishedData = publishedData;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        if(bookTitle.isEmpty()){
+            throw new RuntimeException(" Invalid book title "+ bookTitle);
+        }
+        this.bookTitle = bookTitle;
+    }
+
+    public String getPublishedData() {
+        return publishedData;
+    }
+}
