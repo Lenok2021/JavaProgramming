ac98013a23afb3aa165f533dfb055b8d2df232ba
cross <crosstown11223@yahoo.com>
2022-02-28 12:31:28 -0500

    letSetPractice

--- src/day50CollectionsMap/ListSetPractice.java
+++ src/day50CollectionsMap/ListSetPractice.java
@@ -26,6 +26,28 @@ public class ListSetPractice {
         // if I call  any methods?
         // System.out.println(names.size());
         // NullPointerException
+        System.out.println("----------------------------------------------------");
+
+        //pop () ===>  LIFO
+        List<Character> stack = new Stack<>();
+        stack.addAll(Arrays.asList('A', 'B', 'C', 'D'));
+        System.out.println(stack); //4 elements
+        (   (Stack) stack  ).pop();  // we have  cast  it BC  pop  is a Stack method
+        System.out.println(stack);// 3 elements, the last inserted element was removed
+
+
+
+        //poll()  ===>  FIFO
+
+        List<String> linkedList = new LinkedList<>();
+        linkedList.addAll(Arrays.asList("Tom","Jimmy","Anna","Tom"));
+        System.out.println("linkedList = " + linkedList);
+
+      ((LinkedList<String>) linkedList).poll(); // the  first inserted element will be removed
+
+        System.out.println("linkedList = " + linkedList);
+
+
 
 
 
