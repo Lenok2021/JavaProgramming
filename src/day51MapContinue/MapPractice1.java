b506b10193409d45e47f2010bc3b47b77fa249c1
cross <crosstown11223@yahoo.com>
2022-03-01 15:07:53 -0500

    Find Max  and Min Interview

---
+++ src/day51MapContinue/MapPractice1.java
@@ -0,0 +1,81 @@
+package day51MapContinue;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapPractice1 {
+
+    public static void main(String[] args) {
+
+        Map<String, String> employeeMap = new HashMap<>();
+        employeeMap.put("Ddene", "F");
+        employeeMap.put("Winfred", "M");
+        employeeMap.put("Jereme", "M");
+        employeeMap.put("Glad", "F");
+        employeeMap.put("Desirae", "F");
+        employeeMap.put("Kakalina", "F");
+        employeeMap.put("Bertrando", "M");
+        employeeMap.put("Ajay", "F");
+        employeeMap.put("Brigitte", "F");
+        employeeMap.put("Derk", "M");
+        employeeMap.put("Orlando", "M");
+        employeeMap.put("Selle", "F");
+        employeeMap.put("Marika", "F");
+        System.out.println("employeeMap = " + employeeMap);
+
+        //  1. Update the "M" to Male and "F" to Female
+        // if  you have  a Key  you  can gey a value
+        // at first we need to get each key  we  iterated Map  by using Key
+        for (String key : employeeMap.keySet()) {
+
+            if (employeeMap.get(key).equalsIgnoreCase("m")) {
+                employeeMap.replace(key, "Male");
+            }
+            if (employeeMap.get(key).equalsIgnoreCase("f")) {
+                employeeMap.replace(key, "Female");
+            }
+
+        }
+        System.out.println("employeeMap = " + employeeMap);
+
+
+        System.out.println("==================================================================");
+
+
+        // 2. iterate by using entrySet () method
+
+        for (Map.Entry<String, String> entry : employeeMap.entrySet()) {
+            System.out.println(entry);
+
+            if (entry.getValue().equalsIgnoreCase("m")) {
+                entry.setValue("Male");
+                //  employeeMap.replace(entry.getKey(), "Male");
+
+
+            }
+            if (entry.getValue().equalsIgnoreCase("f")) {
+                entry.setValue("Female");
+                //  employeeMap.replace(entry.getKey(), "Male");
+
+
+            }
+
+        }
+        System.out.println("========================================================");
+
+// 2. Display  the name of all female  Employees
+
+        for (Map.Entry<String, String> pair : employeeMap.entrySet()) {
+
+            String eachKey = pair.getKey(); // represents each name
+            String eachValue = pair.getValue();  //  presents gender
+            if(eachValue.equals("Female")){
+                System.out.println(eachKey);
+            }
+
+        }
+
+
+    }
+
+}
---
+++ src/day51MapContinue/MapPractice3.java
@@ -0,0 +1,44 @@
+package day51MapContinue;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapPractice3 {
+
+    public static void main(String[] args) {
+
+        // displays
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("John", 123000);
+        map.put("Antony", 100000);
+        map.put("Jimmy", 115000);
+        map.put("James", 110000);
+        map.put("Conor", 85000);
+        map.put("Josh", 117000);
+        map.put("Cory", 118000);
+        map.put("Anderson", 125000);
+        map.put("Steven", 135000);
+
+
+        //Who has the maximum salary?
+        String name1 = "";
+        int maxSalary = Integer.MAX_VALUE;
+
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+
+            String eachKey = pair.getKey();
+            int eachValue = pair.getValue();
+
+            if (eachValue > maxSalary) {
+                maxSalary = eachValue;
+                name1 = eachKey;
+            }
+            System.out.println(name1);
+
+
+        }
+
+
+    }
+}
