fb41e0d8eb4041ae8593783ecb315e1fec2f7012
cross <crosstown11223@yahoo.com>
2022-03-01 15:12:53 -0500

    Find Max  and Min Interview

--- src/day51MapContinue/MapPractice3.java
+++ src/day51MapContinue/MapPractice3.java
@@ -1,5 +1,6 @@
 package day51MapContinue;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -7,37 +8,45 @@ public class MapPractice3 {
 
     public static void main(String[] args) {
 
-        // displays
+        // displays all Employee  who  made  the MAX SALARY
+
 
         Map<String, Integer> map = new LinkedHashMap<>();
         map.put("John", 123000);
         map.put("Antony", 100000);
         map.put("Jimmy", 115000);
-        map.put("James", 110000);
+        map.put("Jalil", 145000);
+        map.put("James", 145000);
         map.put("Conor", 85000);
-        map.put("Josh", 117000);
-        map.put("Cory", 118000);
-        map.put("Anderson", 125000);
+        map.put("Josh", 145000);
+        map.put("Cory", 145000);
+        map.put("Anderson",125000);
         map.put("Steven", 135000);
 
+        // displays the names of all employess who has the maximum salaries
+/*
+        int maxSalary = Integer.MIN_VALUE;
+        for ( Integer eachValue : map.values() ) {
+            if(eachValue > maxSalary){
+                maxSalary = eachValue;
+            }
+        }
+*/
 
-        //Who has the maximum salary?
-        String name1 = "";
-        int maxSalary = Integer.MAX_VALUE;
+        int maxSalary = Collections.max( map.values() );
+
+        System.out.println("maxSalary = " + maxSalary);
 
         for (Map.Entry<String, Integer> pair : map.entrySet()) {
-
-            String eachKey = pair.getKey();
-            int eachValue = pair.getValue();
-
-            if (eachValue > maxSalary) {
-                maxSalary = eachValue;
-                name1 = eachKey;
+            if(pair.getValue() == maxSalary){
+                System.out.println(pair.getKey());
             }
-            System.out.println(name1);
-
-
         }
+
+
+
+
+
 
 
     }
