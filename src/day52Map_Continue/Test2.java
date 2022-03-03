e44a1cd4164debe7b3240e7a96edc36dc811e981
cross <crosstown11223@yahoo.com>
2022-03-03 13:08:44 -0500

    ArrayOfMap

--- src/day52Map_Continue/Test2.java
+++ src/day52Map_Continue/Test2.java
@@ -4,14 +4,15 @@ public class Test2 {
 
     public static void main(String[] args) {
 
+        SecondFunctionalInterface<String> printEach;
+        printEach = s -> {
+            for (int i = 0; i <= s.length() - 1; i++) {
+                System.out.println(s.charAt(i));
+            }
 
-
-
-
-
+        };
+        printEach.test("Hello");
     }
-
-
 
 
 }
