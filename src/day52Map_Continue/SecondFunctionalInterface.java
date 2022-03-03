1ac85adee663a70acece1bc1226e4ba730eba353
cross <crosstown11223@yahoo.com>
2022-03-03 13:00:43 -0500

    ArrayOfMap

--- src/day52Map_Continue/SecondFunctionalInterface.java
+++ src/day52Map_Continue/SecondFunctionalInterface.java
@@ -1,8 +1,15 @@
 package day52Map_Continue;
 
-public interface SecondFunctionalInterface {
+
+@java.lang.FunctionalInterface
+public interface SecondFunctionalInterface<T> {
+
+      // we want  to create FI, who is generic
+      // we can give latter generic type
+      // we want method, which is working with any data type
 
 
+    void test(T data);
 
 
 }
---
+++ src/day52Map_Continue/Test2.java
@@ -0,0 +1,17 @@
+package day52Map_Continue;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+
+
+
+}
