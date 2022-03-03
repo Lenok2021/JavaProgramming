789cb3c2ca19409ec400fb5c9e9c4f79b77550fd
cross <crosstown11223@yahoo.com>
2022-03-03 12:26:22 -0500

    ArrayOfMap

---
+++ src/day52Map_Continue/FunctionalInterface.java
@@ -0,0 +1,24 @@
+package day52Map_Continue;
+
+
+
+@java.lang.FunctionalInterface // annotation
+public interface FunctionalInterface {
+
+
+    void apply(int n);
+
+   // void method();  only one method allowed
+
+
+
+    // Difference between regular Interface and FI:
+    //1. Functional Interface = creating a function = method
+    //2. how provide implement = Lambda Exp.
+    //3. not for inheritance purpose
+    //4. FI can only have 1 abstract method
+
+
+    //regular I  provides add. methods,  implementation = method overriding
+
+}
